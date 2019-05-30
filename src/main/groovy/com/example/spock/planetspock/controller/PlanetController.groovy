137fe3cbe20870016253aaac7cf885e51e66bc9b
oanhptl <oanhptl@eway.vn>
2019-05-30 16:14:18 +0700

    first commit

--- src/main/groovy/com/example/spock/planetspock/controller/PlanetController.groovy
+++ src/main/groovy/com/example/spock/planetspock/controller/PlanetController.groovy
@@ -2,13 +2,20 @@ package com.example.spock.planetspock.controller
 
 import com.example.spock.planetspock.model.Planet
 import com.example.spock.planetspock.serviece.PlanetService
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
+import org.springframework.web.servlet.View
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import org.springframework.web.servlet.view.RedirectView
 
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @RestController
@@ -25,4 +32,23 @@ class PlanetController {
     public Planet retrieve(@PathVariable("planetName") String name) {
         return this.planetService.findOneByName(name)
     }
+
+    @GetMapping("/redirectWithRedirectView")
+        public RedirectView redirectWithUsingRedirectView(
+            RedirectAttributes attributes) {
+            attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
+            attributes.addAttribute("attribute", "redirectWithRedirectView");
+            return new RedirectView("redirectedUrl");
+        }
+    @PostMapping("/redirectPostToPost")
+    public ModelAndView redirectPostToPost(HttpServletRequest request) {
+        request.setAttribute(
+                View.RESPONSE_STATUS_ATTRIBUTE, HttpStatus.TEMPORARY_REDIRECT)
+        return new ModelAndView("redirect:/redirectedPostToPost");
+    }
+    @PostMapping("/redirectedPostToPost")
+    public ModelAndView redirectedPostToPost() {
+        return new ModelAndView("redirection");
+    }
+
 }
